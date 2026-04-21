be5bfcc8ff9f1bb04a3f98b2a7d8d12f5e4f6d52
Matthew Fay <mmfay3@gmail.com>
2026-04-21 04:12:33 -0700

    Add Entity & Tenant Tables

--- backend/ddl/schema.sql
+++ backend/ddl/schema.sql
@@ -6,13 +6,48 @@ CREATE TABLE IF NOT EXISTS users (
     user_id TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL,
     version_id INTEGER NOT NULL DEFAULT 1,
+	tenant_id INTEGER NOT NULL,
+	default_company_id INTEGER NULL,
 	is_enabled BOOLEAN NOT NULL DEFAULT FALSE
 );
 
-INSERT INTO users (email, user_id, password, is_enabled)
+DROP TABLE IF EXISTS sessions;
+
+CREATE TABLE IF NOT EXISTS sessions (
+	id SERIAL PRIMARY KEY,
+	user_id TEXT NOT NULL,
+	email TEXT NOT NULL,
+	tenant_id INTEGER NOT NULL,
+	company_id INTEGER NULL,
+	created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+	expires_at TIMESTAMPTZ NOT NULL,
+	is_active BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+DROP TABLE IF EXISTS tenants;
+
+CREATE TABLE IF NOT EXISTS tenants (
+	id SERIAL PRIMARY KEY,
+	name TEXT NOT NULL,
+	email TEXT NOT NULL,
+	created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+	is_active BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+DROP TABLE IF EXISTS entities;
+
+CREATE TABLE IF NOT EXISTS entities (
+	id SERIAL PRIMARY KEY,
+	tenant_id INTEGER NOT NULL,
+	name TEXT NOT NULL,
+	created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+	is_active BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+INSERT INTO users (email, user_id, password, is_enabled, tenant_id)
 VALUES
-    ('admin@axiom.com', 'admin', 'hashed_password_1', TRUE),
-    ('john.doe@axiom.com', 'jdoe', 'hashed_password_2', FALSE),
-    ('jane.smith@axiom.com', 'jsmith', 'hashed_password_3', TRUE),
-    ('michael.brown@axiom.com', 'mbrown', 'hashed_password_4', TRUE),
-    ('sarah.wilson@axiom.com', 'swilson', 'hashed_password_5', TRUE);
+    ('admin@softwarerror.com', 'admin', '$2b$12$jUzsxGMk3ES8VBpRWKvW.uYwZgEbKJHUo0/0G3Fi4AsGy.GhaxW2u', TRUE, 1),
+    ('john.doe@softwarerror.com', 'jdoe', '$2b$12$jUzsxGMk3ES8VBpRWKvW.uYwZgEbKJHUo0/0G3Fi4AsGy.GhaxW2u', FALSE, 1),
+    ('jane.smith@softwarerror.com', 'jsmith', '$2b$12$jUzsxGMk3ES8VBpRWKvW.uYwZgEbKJHUo0/0G3Fi4AsGy.GhaxW2u', TRUE, 1),
+    ('michael.brown@softwarerror.com', 'mbrown', '$2b$12$jUzsxGMk3ES8VBpRWKvW.uYwZgEbKJHUo0/0G3Fi4AsGy.GhaxW2u', TRUE, 1),
+    ('sarah.wilson@softwarerror.com', 'swilson', '$2b$12$jUzsxGMk3ES8VBpRWKvW.uYwZgEbKJHUo0/0G3Fi4AsGy.GhaxW2u', TRUE, 1);
