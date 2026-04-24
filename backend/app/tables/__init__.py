1a5de9fd8e3cbe96b09eec6acd65a1d98e4a4832
Matthew Fay <mmfay3@gmail.com>
2026-04-24 04:32:10 -0700

    Add init to tables

---
+++ backend/app/tables/__init__.py
@@ -0,0 +1,13 @@
+from .Users import Users
+from .Entities import Entities
+from .Sessions import Sessions
+from .Tenants import Tenants
+from .Tokens import Tokens
+
+__all__ = [
+	"Users",
+	"Tokens",
+	"Entities",
+	"Tenants",
+	"Sessions",
+]
